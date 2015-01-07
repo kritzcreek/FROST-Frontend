75899f69954f6a562a8ccd606c7277310de066dd
kRITZCREEK <christoph.hegemann@gmx.net>
2015-01-07 22:50:47 +0100

    Formatting.

--- static/topics.js
+++ static/topics.js
@@ -5,31 +5,32 @@ var Panel = ReactBootstrap.Panel;
 Topic = React.createClass({
   handleClick: function(e) {
     var event = new CustomEvent('clickTopic',
-				{ 'detail': this.props.topic });
+                                { 'detail': this.props.topic });
     this.props.emit(event);
   },
   handleDragStart: function(e) {
+    console.log(this.props.topic)
     var event = new CustomEvent('dragStartTopic',
-				{ 'detail': this.props.topic});
+                                { 'detail': this.props.topic});
     this.props.emit(event);
   },
   handleDragEnd: function(e) {
     var event = new CustomEvent('dragEndTopic',
-				{ 'detail': this.props.topic});
+                                { 'detail': this.props.topic});
     this.props.emit(event);
   },
   render: function(){
     return (
-	<div
+    <div
       className={"topic"}
       onClick={this.handleClick}
       draggable="true"
       onDragStart={this.handleDragStart}
       onDragEnd={this.handleDragEnd}
-	>
+    >
         <div className="description"> Thema: {this.props.topic.description}</div>
         <div className="typ"> Typ: {this.props.topic.typ}</div>
-	</div>
+    </div>
     );
   }
 });
@@ -41,17 +42,16 @@ Topics = React.createClass({
   render: function(){
     var topics = this.props.topics
       .map(function(topic){
-	return (
-            <Topic topic={topic} key={topic.description} emit={this.emit}>
-	    </Topic>
-	);
+        return (
+            <Topic topic={topic} key={topic.description} emit={this.emit} />
+        );
       },this);
     return (
-	<Panel id="topicsContainer" header="Themen" bsStyle="primary">
-	<div id="topic">
-	{topics}
-      </div>
-	</Panel>
+        <Panel id="topicsContainer" header="Themen" bsStyle="primary">
+        <div id="topic">
+          {topics}
+        </div>
+        </Panel>
     );
   }
 });
