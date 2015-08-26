d9c3a0432c3bd9a9bacf97cc0e5c6b31df31773f
creek <christoph.hegemann@gmx.net>
2015-08-27 01:04:50 +0200

    configuration for linter and bundling

--- webpack.config.js
+++ webpack.config.js
@@ -5,32 +5,32 @@ var DedupePlugin = new webpack.optimize.DedupePlugin();
 var UglifyJsPlugin = new webpack.optimize.UglifyJsPlugin();
 
 module.exports = {
-    entry: './static/js/entry.js',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'js/app.js'
-    },
-    module: {
-        loaders: [{
-            test: /\.purs$/,
-            loader: 'purs',
-            query: {
-                output: 'output'
-            }
-        }, {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel'
-        }]
-    },
-    resolve: {
-        modulesDirectories: [
-            'node_modules',
-            'web_modules',
-            'output'
-        ]
-    }
-    // Uncomment for production build
-    /*, plugins: [DedupePlugin, UglifyJsPlugin]*/
+  entry: './static/js/entry.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'js/app.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.purs$/,
+      loader: 'purs',
+      query: {
+        output: 'output'
+      }
+    }, {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    modulesDirectories: [
+      'node_modules',
+      'web_modules',
+      'output'
+    ]
+  }
+  // Uncomment for production build
+  /* , plugins: [DedupePlugin, UglifyJsPlugin]*/
 };
 
